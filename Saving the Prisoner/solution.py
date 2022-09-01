9edb15d36753b63f94290a846bfee79269ab8e2c
SHREEYASH MAHAJAN <103109932+Shreeyash01@users.noreply.github.com>
2022-09-01 22:39:01 +0530

    Create solution.py

---
+++ Saving the Prisoner/solution.py
@@ -0,0 +1,13 @@
+n=int(input())
+for i in range(n):
+    a=list(map(int,input().split()))
+    if a[1]>a[0]:
+        e=(a[0]+1)-a[2]
+        a[1]=a[1]-e
+        while(a[1]>a[0]):
+            a[1]=a[1]-a[0]
+        print(a[1])
+    else:
+        e=(a[2]-1)+a[1]
+        print(e)
+        

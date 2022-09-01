23c830832067c8543540013e257ccf416160eaea
SHREEYASH MAHAJAN <103109932+Shreeyash01@users.noreply.github.com>
2022-09-01 22:41:54 +0530

    Update solution.py

--- Saving the Prisoner/solution.py
+++ Saving the Prisoner/solution.py
@@ -1,13 +1,17 @@
+# n=int(input())
+# for i in range(n):
+#     a=list(map(int,input().split()))
+#     if a[1]>a[0]:
+#         e=(a[0]+1)-a[2]
+#         a[1]=a[1]-e
+#         while(a[1]>a[0]):
+#             a[1]=a[1]-a[0]
+#         print(a[1])
+#     else:
+#         e=(a[2]-1)+a[1]
+#         print(e)
+        
 n=int(input())
 for i in range(n):
     a=list(map(int,input().split()))
-    if a[1]>a[0]:
-        e=(a[0]+1)-a[2]
-        a[1]=a[1]-e
-        while(a[1]>a[0]):
-            a[1]=a[1]-a[0]
-        print(a[1])
-    else:
-        e=(a[2]-1)+a[1]
-        print(e)
-        
+    print((a[1]+a[2]-2)%a[0] +1)

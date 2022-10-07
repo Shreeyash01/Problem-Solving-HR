2a1545adbd1bc53bcef8d426dd1222037b018299
SHREEYASH MAHAJAN <103109932+Shreeyash01@users.noreply.github.com>
2022-10-07 23:53:35 +0530

    Update Solution.py

--- Chocolate Feast/Solution.py
+++ Chocolate Feast/Solution.py
@@ -1,10 +1,13 @@
 l = int(input())
+
 for i in range(l):
     n, c, m = [int(r) for r in input().split()]
     t=n//c
     a=t
+    
     if a < m:
         print(t)
+        
     else:
         while (a >= m):
             temp = a // m

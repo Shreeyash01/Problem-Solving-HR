fe831afd107a5cfcbdc26a018421fdcc326987df
SHREEYASH MAHAJAN <103109932+Shreeyash01@users.noreply.github.com>
2022-10-20 21:32:27 +0530

    Update Solution.py

--- Cut the Sticks/Solution.py
+++ Cut the Sticks/Solution.py
@@ -1,9 +1,9 @@
-n=int(input())
-a=list(map(int,input().split()))
+p=int(input())
+b=list(map(int,input().split()))
 
-while(len(a)>0) :
-    print(len(a))
-    m=min(a)
-    for i in range(len(a)):
-        a[i]=a[i]-m
-    a = list(filter((0).__ne__, a))
+while(len(b)>0) :
+    print(len(b))
+    m=min(b)
+    for i in range(len(b)):
+        b[i]=b[i]-m
+    b = list(filter((0).__ne__, b))

0a2344bd581ce0d59892339cbbd1390a5b1a6fdc
SHREEYASH MAHAJAN <103109932+Shreeyash01@users.noreply.github.com>
2022-11-02 23:20:46 +0530

    Update solution.py

--- Find Digits/solution.py
+++ Find Digits/solution.py
@@ -1,12 +1,12 @@
-n=int(input())
-for i in range(n):
-    s=input()
+m=int(input())
+for i in range(m):
+    a=input()
     cnt=0
-    for i in range(len(s)):
-        if int(s[i])==0 :
+    for i in range(len(a)):
+        if int(a[i])==0 :
             pass
         else:
-            if (int(s) % int(s[i]) == 0) :
+            if (int(a) % int(a[i]) == 0) :
                 cnt=cnt+1
             else:
                 continue
